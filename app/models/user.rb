94793f0ba16b00ec94fdeeb5a11fe00148068c01
Chris "Miles <cmiles@orangebarrelmedia.com>
2022-05-01 11:52:01 -0400

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,9 @@ class User < ApplicationRecord
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false },
                        length: { minimum: 3, maximum: 25 }
-
-  VAILID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false },
                     length: { maximum: 105 },
-                    format: { with: VAILID_EMAIL_REGEX }
+                    format: { with: VALID_EMAIL_REGEX }
 end
